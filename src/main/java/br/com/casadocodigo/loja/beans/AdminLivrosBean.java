4b2715362f58aee2c09cf70a447285d40d6564aa
e539740 <e539740@DF5142ET111.corp.caixa.gov.br>
2019-07-30 13:13:56 -0300

    configuração inicial do projeto

---
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -0,0 +1,9 @@
+package br.com.casadocodigo.loja.beans;
+
+public class AdminLivrosBean {
+    private Object livro;
+
+    public void salvar() {
+        System.out.println("Livro salvo com Sucesso!");
+    }
+}
