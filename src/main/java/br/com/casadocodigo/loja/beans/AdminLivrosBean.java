a1d389d6c46b242746fe166eff283147f8cc0f4b
e539740 <e539740@DF5142ET111.corp.caixa.gov.br>
2019-07-31 13:35:25 -0300

    Seguindo aula 2, criado model de autores, ajustes no form e tela lista

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,26 +1,42 @@
 package br.com.casadocodigo.loja.beans;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.transaction.Transactional;
 
+import br.com.casadocodigo.loja.daos.AutorDao;
 import br.com.casadocodigo.loja.daos.LivroDao;
+import br.com.casadocodigo.loja.models.Autor;
 import br.com.casadocodigo.loja.models.Livro;
 
 @Named
 @RequestScoped
 public class AdminLivrosBean {
 
-	private Livro livro = new Livro();
-	
 	@Inject
 	private LivroDao dao;
+	@Inject
+	private AutorDao autorDao;
+
+	private Livro livro = new Livro();
+	private List<Integer> autoresId = new ArrayList<>();
 
 	@Transactional
-	public void salvar() {
-		System.out.println("Livro cadastrado " + livro);
+	public String salvar() {
+		autoresId.stream().forEach(id -> livro.getAutores().add(new Autor(id)));
 		dao.salvar(livro);
+		
+		System.out.println("Livro cadastrado " + livro);
+		
+		return "/livros/lista?faces-redirect=true";
+	}
+
+	public List<Autor> getAutores() {
+		return autorDao.listar();
 	}
 
 	public Livro getLivro() {
@@ -29,5 +45,13 @@ public class AdminLivrosBean {
 
 	public void setLivro(Livro livro) {
 		this.livro = livro;
+	}
+
+	public List<Integer> getAutoresId() {
+		return autoresId;
+	}
+
+	public void setAutoresId(List<Integer> autoresId) {
+		this.autoresId = autoresId;
 	}
 }
