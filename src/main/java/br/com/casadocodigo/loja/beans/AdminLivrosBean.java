3ad4c4b36f1ab6d75c5cf317337c4cb523f6a874
Geraldo Oliveira <geraldocoosta@gmail.com>
2019-07-30 21:24:08 -0300

    Terminando a primeira aula do curso de javaee

--- src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -1,9 +1,33 @@
 package br.com.casadocodigo.loja.beans;
 
-public class AdminLivrosBean {
-    private Object livro;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
 
-    public void salvar() {
-        System.out.println("Livro salvo com Sucesso!");
-    }
+import br.com.casadocodigo.loja.daos.LivroDao;
+import br.com.casadocodigo.loja.models.Livro;
+
+@Named
+@RequestScoped
+public class AdminLivrosBean {
+
+	private Livro livro = new Livro();
+	
+	@Inject
+	private LivroDao dao;
+
+	@Transactional
+	public void salvar() {
+		System.out.println("Livro cadastrado " + livro);
+		dao.salvar(livro);
+	}
+
+	public Livro getLivro() {
+		return livro;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
 }
---
+++ src/main/java/br/com/casadocodigo/loja/models/Livro.java
@@ -0,0 +1,61 @@
+package br.com.casadocodigo.loja.models;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class Livro {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String titulo;
+	@Lob
+	private String descricao;
+	private BigDecimal preco;
+	private Integer numeroDePaginas;
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Integer getNumeroDePaginas() {
+		return numeroDePaginas;
+	}
+
+	public void setNumeroDePaginas(Integer numeroDePaginas) {
+		this.numeroDePaginas = numeroDePaginas;
+	}
+
+	@Override
+	public String toString() {
+		return "Livro [titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco + ", numeroDePaginas="
+				+ numeroDePaginas + "]";
+	}
+
+}
